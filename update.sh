1bc3e612c78906e3ea2afbeda1cc7ae27dee7adf
NoryGhost <105328139+NoryGhost@users.noreply.github.com>
2022-06-03 16:30:13 +0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo m----m-mmmmm--mmmm-----mm--mmmmmmm-mmmmmm-mmmmm
+echo m----m-m---mm-m---mm---mm---- m--- m------m----mm
+echo m----m-mmmmmm-m----m--m--m----m----mmmmmm-mmmmmm
+echo m----m-m------m----m--mmmm----m----m------m---mm
+echo mmmmmm-m------mmmmm--m----m---m----mmmmmm-m-----m
+
+pacman -Syu
+
+pacman -Sc
+
+echo m-----m-mmmmmm-m------m
+echo m--m--m-m------m------m
+echo m-mmm-m-mmmmmm-m------m
+echo -mm-mmm-m------m------m
+echo -m---m--mmmmmm-mmmmmm-mmmmmm---m---m---m
+
+sleep 3
